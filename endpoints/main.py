0bc9d467a7be4ee61d5a7a17cdc699873a7c0bd3
FabFari <FabrizioFarinacci@gmail.com>
2015-02-03 16:21:19 +0100

    Endpoints Updated
    Aggiunta tabella artisti con funzioni di manipolazione, modificata
    tabella artworks, aggiunta query artwork di un artista

--- endpoints/main.py
+++ endpoints/main.py
@@ -58,9 +58,14 @@ class IndexNumber:
         return vmax + 1
 
 
+# Messages Classes
 class UploadRequestMessage(messages.Message):
     filename = messages.StringField(1, required=True)
     photo = messages.BytesField(2, required=True)
+    artist = messages.StringField(3, required=True)
+    descr = messages.StringField(4)
+    luogo = messages.StringField(5)
+    dim = messages.StringField(6)
 
 class DownloadRequestMessage(messages.Message):
     fetch = messages.IntegerField(1, required=True)
@@ -70,6 +75,11 @@ class DownloadResponseMessage(messages.Message):
     filename = messages.StringField(1, required=True)
     date_time = messages.StringField(2, required=True)
     photo = messages.StringField(3, required=True)
+    artist = messages.StringField(4, required=True)
+    descr = messages.StringField(5)
+    luogo = messages.StringField(6)
+    dim = messages.StringField(7)
+    likes = messages.IntegerField(8)
 
 class DownloadResponseCollection(messages.Message):
     photos = messages.MessageField(DownloadResponseMessage, 1, repeated=True)
@@ -78,14 +88,85 @@ class DeleteRequestMessage(messages.Message):
     filename = messages.StringField(1, required=True)
     is_ndb_entry = messages.BooleanField(2, required=True)
 
-class DeleteResponseMessage(messages.Message):
+class PictureDetailsMessage(messages.Message):
+    gcs_filename = messages.StringField(1, required=True)
+    filename = messages.StringField(2, required=True)
+    date_time = messages.StringField(3, required=True)
+    photo = messages.StringField(4, required=True)
+    artist = messages.StringField(5, required=True)
+    descr = messages.StringField(6)
+    luogo = messages.StringField(7)
+    dim = messages.StringField(8)
+    likes = messages.IntegerField(9)
+
+class PictureUpdateMessage(messages.Message):
+    gcs_filename = messages.StringField(1, required=True)
+    filename = messages.StringField(2)
+    descr = messages.StringField(3)
+    luogo = messages.StringField(4)
+    dim = messages.StringField(5)
+
+class PictureDetailsCollection(messages.Message):
+    photos = messages.MessageField(PictureDetailsMessage, 1, repeated=True)
+
+class ArtistInfoMessage(messages.Message):
+    email = messages.StringField(1, required=True)
+    nome = messages.StringField(2, required=True)
+    cognome = messages.StringField(3, required=True)
+    nickname = messages.StringField(4)
+    sito = messages.StringField(5)
+    bio = messages.StringField(6)
+    pic = messages.StringField(7)
+
+class ArtistUpdateMessage(messages.Message):
+    email = messages.StringField(1, required=True)
+    new_email = messages.StringField(2)
+    new_nome = messages.StringField(3)
+    new_cognome = messages.StringField(4)
+    new_nickname = messages.StringField(5)
+    new_sito = messages.StringField(6)
+    new_bio = messages.StringField(7)
+    new_pic = messages.StringField(8)
+
+class ArtistDetailsMessage(messages.Message):
+    message = messages.StringField(1)
+    email = messages.StringField(2)
+    nome = messages.StringField(3)
+    cognome = messages.StringField(4)
+    nickname = messages.StringField(5)
+    sito = messages.StringField(6)
+    bio = messages.StringField(7)
+    pic = messages.StringField(8)
+
+class ArtistRequestMessage(messages.Message):
+    email = messages.StringField(1, required=True)
+
+class DefaultResponseMessage(messages.Message):
     message = messages.StringField(1)
 
+# Datastore Tables
 class PictureEntry(ndb.Model):
-    blob_key = ndb.StringProperty()
+    blob_key = ndb.StringProperty(required=True)
     date_time = ndb.DateTimeProperty(auto_now_add=True)
-    filename = ndb.StringProperty()
-    gcs_filename = ndb.StringProperty()
+    filename = ndb.StringProperty(required=True)
+    artist = ndb.StringProperty(required=True)
+    gcs_filename = ndb.StringProperty(required=True)
+    descr = ndb.StringProperty()
+    luogo = ndb.StringProperty()
+    dim = ndb.StringProperty()
+    likes = ndb.IntegerProperty()
+
+class ArtistEntry(ndb.Model):
+    email = ndb.StringProperty(required=True)
+    nome = ndb.StringProperty(required=True)
+    cognome = ndb.StringProperty(required=True)
+    nickname = ndb.StringProperty()
+    sito = ndb.StringProperty()
+    bio = ndb.StringProperty()
+    pic = ndb.StringProperty()
+
+class TechniqueEntry(ndb.Model):
+    nome = ndb.StringProperty(required=True)
 
 @endpoints.api(name="testGCS", version="v1",
                allowed_client_ids=[WEB_CLIENT_ID, ANDROID_CLIENT_ID,
@@ -93,11 +174,10 @@ class PictureEntry(ndb.Model):
                audiences=[ANDROID_AUDIENCE],
                scopes=[endpoints.EMAIL_SCOPE])
 class TestGCS(remote.Service):
-
     def __init__(self):
         self.num_index = IndexNumber()
 
-    @endpoints.method(UploadRequestMessage, message_types.VoidMessage,
+    @endpoints.method(UploadRequestMessage, DefaultResponseMessage,
                       path='upload', http_method="POST", name="upload.putphoto")
     def upload_putphoto(self, request):
         filename = request.filename
@@ -149,12 +229,19 @@ class TestGCS(remote.Service):
 
         bs_filename = "/gs" + gcs_filename
         blob_key = blobstore.create_gs_key(bs_filename)
-        pic = PictureEntry(blob_key=blob_key, filename=filename, gcs_filename=gcs_filename)
+
+        artist = ArtistEntry.query(ArtistEntry.email == request.artist).get()
+
+        if artist is None:
+            return DefaultResponseMessage(message="Artist not found!")
+
+        pic = PictureEntry(parent=artist.key, blob_key=blob_key, filename=filename, gcs_filename=gcs_filename,
+                           artist=request.artist, descr=request.descr, luogo=request.luogo, dim=request.dim, likes=0)
         pic.put()
 
         num_index.inc_index()
 
-        return message_types.VoidMessage()
+        return DefaultResponseMessage(message="Artwork added!")
 
     @endpoints.method(DownloadRequestMessage, DownloadResponseCollection,
                       path='display', http_method="GET", name="display.getphotos")
@@ -164,10 +251,8 @@ class TestGCS(remote.Service):
         logging.info("DATE_TIME = %s" % request.date_time)
 
         if request.date_time is None:
-            logging.info("NELL'IF!!!!!!!")
             pictures = PictureEntry.query().order(-PictureEntry.date_time).fetch(fetch)
         else:
-            logging.info("NELL'ELSE!!!!!!")
             date_time = datetime.strptime(request.date_time, "%Y-%m-%d %H:%M:%S")
             pictures = PictureEntry.query(PictureEntry.date_time < date_time).order(-PictureEntry.date_time).fetch(fetch)
 
@@ -177,11 +262,13 @@ class TestGCS(remote.Service):
             imgurl = images.get_serving_url(blob_key=pic.blob_key)
             date_timeobj = pic.date_time
             date_timestr = date_timeobj.strftime("%Y-%m-%d %H:%M:%S")
-            messlist.append(DownloadResponseMessage(filename=pic.filename, date_time=date_timestr, photo=imgurl))
+            messlist.append(DownloadResponseMessage(artist=pic.artist, filename=pic.filename, date_time=date_timestr,
+                                                    photo=imgurl, descr=pic.descr, luogo=pic.luogo, dim=pic.dim,
+                                                    likes=pic.likes))
 
         return DownloadResponseCollection(photos=messlist)
 
-    @endpoints.method(DeleteRequestMessage, DeleteResponseMessage,
+    @endpoints.method(DeleteRequestMessage, DefaultResponseMessage,
                       path='delete', http_method='GET', name='delete.deletephoto')
     def delete_deletephoto(self, request):
         filename = request.filename
@@ -200,7 +287,111 @@ class TestGCS(remote.Service):
         except gcs.NotFoundError:
             return DownloadResponseMessage(message="File Not Found!!!")
 
-        return DeleteResponseMessage(message="File removed.")
+        return DefaultResponseMessage(message="File removed.")
+
+    @endpoints.method(ArtistInfoMessage, DefaultResponseMessage,
+                      path='registration', http_method='GET', name='registration.registerartist')
+    def register_artist(self, request):
+        artist = ArtistEntry.query(ArtistEntry.email == request.email)
+
+        if artist.get() is not None:
+            return DefaultResponseMessage(message="Artist already exists!")
+
+
+        email = request.email
+        nome = request.nome
+        cognome = request.cognome
+        nickname = request.nickname
+        sito = request.sito
+        bio = request.bio
+        pic = request.pic
+
+        artist = ArtistEntry(email=email, nome=nome, cognome=cognome,
+                             nickname=nickname, sito=sito, bio=bio, pic=pic)
+
+        artist.put()
+
+        return DefaultResponseMessage(message="Artist registered")
+
+    @endpoints.method(ArtistRequestMessage, ArtistDetailsMessage,
+                      path='getinfo', http_method='GET', name='getinfo.getartist')
+    def get_artist(self, request):
+        artist = ArtistEntry.query(ArtistEntry.email == request.email).get()
+
+        if artist is None:
+            return ArtistDetailsMessage("Artist Not Found!")
+
+        return ArtistDetailsMessage(email=artist.email, nome=artist.nome,
+                                    cognome=artist.cognome, nickname=artist.nickname,
+                                    sito=artist.sito, bio=artist.bio, pic=artist.pic)
+
+    @endpoints.method(ArtistUpdateMessage, DefaultResponseMessage,
+                      path='artistinfo', http_method='GET', name='artistinfo.updateartist')
+    def update_artist(self, request):
+        artist = ArtistEntry.query(ArtistEntry.email == request.email).get()
+
+        if artist is None:
+            return DefaultResponseMessage(message="Artist not found!")
+
+        if request.new_email is not None:
+            artist.email = request.new_email
+        if request.new_nome is not None:
+            artist.nome = request.new_nome
+        if request.new_cognome is not None:
+            artist.cognome = request.new_cognome
+        if request.new_nickname is not None:
+            artist.nickname = request.new_nickname
+        if request.new_sito is not None:
+            artist.sito = request.new_sito
+        if request.new_bio is not None:
+            artist.bio = request.new_bio
+        if request.new_pic is not None:
+            artist.pic = request.new_pic
+
+        artist.put()
+
+        return DefaultResponseMessage(message="Artist updated!")
+
+    @endpoints.method(PictureUpdateMessage, DefaultResponseMessage,
+                      path='picinfo', http_method='GET', name='picinfo.updatepicture')
+    def update_picture(self, request):
+        picture = PictureEntry.query(PictureEntry.gcs_filename == request.gcs_filename).get()
+
+        if picture is None:
+            return DefaultResponseMessage(message="Picture not found!")
+
+        if request.filename is not None:
+            picture.filename = request.filename
+        if request.descr is not None:
+            picture.descr = request.descr
+        if request.luogo is not None:
+            picture.luogo = request.luogo
+        if request.dim is not None:
+            picture.dim = request.dim
+
+        picture.put()
+
+        return DefaultResponseMessage(message="Picture Updated!")
+
+
+    @endpoints.method(ArtistRequestMessage, PictureDetailsCollection,
+                      path='artworks', http_method='GET', name='artworks.getartworks')
+    def get_artworks(self, request):
+        artist = ArtistEntry.query(ArtistEntry.email == request.email).get()
+
+        artworks = PictureEntry.query(ancestor=artist.key).order(-PictureEntry.date_time)
+
+        messlist = []
+
+        for pic in artworks:
+            imgurl = images.get_serving_url(blob_key=pic.blob_key)
+            date_timeobj = pic.date_time
+            date_timestr = date_timeobj.strftime("%Y-%m-%d %H:%M:%S")
+            messlist.append(PictureDetailsMessage(artist=pic.artist, gcs_filename=pic.gcs_filename,
+                                                  filename=pic.filename, date_time=date_timestr,photo=imgurl,
+                                                  descr=pic.descr, luogo=pic.luogo, dim=pic.dim, likes=pic.likes))
+
+        return PictureDetailsCollection(photos=messlist)
 
 
 APPLICATION = endpoints.api_server([TestGCS])
